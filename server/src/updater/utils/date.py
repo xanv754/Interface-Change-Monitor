10f97747028cfea1fb5260ff5e536778cc50b6a2
Angxandralol <angyeemarin.dev@gmail.com>
2024-11-19 16:02:58 -0400

    refactor: get date function

--- server/src/updater/utils/date.py
+++ server/src/updater/utils/date.py
@@ -1,9 +1,7 @@
 import datetime
 
 def get_date() -> str:
-    """Return the current date in the format YYYY-MM-DD"""
-    # day = datetime.datetime.now().day - 1
-    day = datetime.datetime.now().day
-    month = datetime.datetime.now().month
-    year = datetime.datetime.now().year
-    return f'{year}-{month}-{day}'
+    """Return the yesterday date in the format YYYY-MM-DD"""
+    today = datetime.date.today()
+    yesterday = today - datetime.timedelta(days=1)
+    return yesterday.strftime("%Y-%m-%d")
