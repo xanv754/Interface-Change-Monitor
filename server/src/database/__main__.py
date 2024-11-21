7b03d1327a2941d696c43d2ccde92015e47ec0c3
Angxandralol <angyeemarin.dev@gmail.com>
2024-11-21 10:50:27 -0400

    feat: add generate to json

--- server/src/database/__main__.py
+++ server/src/database/__main__.py
@@ -3,17 +3,21 @@ from database.models.equipment import EquipmentModel
 from database.models.operator import OperatorModel
 from database.models.interface import InterfaceModel
 from database.models.assignment import AssignmentModel
+from database.utils.json import interface_to_json, equipment_to_json, assignment_to_json, operator_to_json
 
 if __name__ == "__main__":
     try:
         equipment = EquipmentModel.get_equipment_by_id(1)
-        print(equipment)
-        operator = OperatorModel.get_operator('user')
-        print(operator)
+        res = equipment_to_json([equipment])
+        print(res)
         interface = InterfaceModel.get_interface_by_id(1)
-        print(interface)
+        res = interface_to_json([interface])
+        print(res)
+        operator = OperatorModel.get_operator('user1')
+        res = operator_to_json([operator])
+        print(res)
         assignment = AssignmentModel.get_assignment(2, 1, 'user1')
-        print(assignment)
-        print(assignment.model_dump())
+        res = assignment_to_json([assignment])
+        print(res)
     except:
         traceback.print_exc()
