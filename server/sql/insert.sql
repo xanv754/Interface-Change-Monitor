fcb8405b21cb78268cb5102dd17503632eaecd70
Angxandralol <angyeemarin.dev@gmail.com>
2024-12-17 09:01:20 -0400

    refactor: insert sql

--- server/sql/insert.sql
+++ server/sql/insert.sql
@@ -22,12 +22,14 @@ VALUES
   (182, 1, '2023-11-18', 'YESTERDAY', 'GigabitEthernet0/1', 'Interface 1', 'GE0/1', 100, 1000, '00:00:00:00:00:00', 'ethernetCsmacd', 'DOWN', 'UP', FALSE, FALSE, '0:00:00.00'),
   (100, 1, '2023-11-19', 'TODAY', 'GigabitEthernet0/1', 'Interface 1', 'GE0/1', 100, 1000, '00:00:00:00:00:00', 'ethernetCsmacd', 'UP', 'UP', FALSE, FALSE, '0:00:00.00');
 
-INSERT INTO operator (username, name, lastname, password, profile, statusAccount, deleteOperator)
+INSERT INTO operator (username, name, lastname, password, profile, statusAccount)
 VALUES
-  ('user1', 'Manuel', 'Peterson', 'secret123', 'STANDARD', 'ACTIVE', FALSE),
-  ('user2', 'Gerardo', 'Gonzalez', 'secret123', 'ADMIN', 'ACTIVE', FALSE),
-  ('user3', 'Patricio', 'Estrella', 'fondoDeBikini', 'SUPERADMIN', 'ACTIVE', FALSE),
-  ('user4', 'Wilman', 'Eugenio', 'disleixaAProposito', 'SUPERADMIN', 'INACTIVE', FALSE);
+  ('user', 'Pepe', 'Perez', 'secret123', 'STANDARD', 'ACTIVE'),
+  ('user1', 'Manuel', 'Peterson', 'secret123', 'STANDARD', 'ACTIVE'),
+  ('user2', 'Gerardo', 'Gonzalez', 'secret123', 'ADMIN', 'ACTIVE'),
+  ('user3', 'Patricio', 'Estrella', 'fondoDeBikini', 'SUPERADMIN', 'DELETED'),
+  ('user4', 'Wilman', 'Eugenio', 'disleixaAProposito', 'SUPERADMIN', 'INACTIVE'),
+  ('USER5', 'Angyee', 'Marin', '123', 'SUPERADMIN', 'ACTIVE');
 
 INSERT INTO assignment (changeInterface, oldInterface, operator, dateAssignment, statusAssignment, assignedBy)
 VALUES
