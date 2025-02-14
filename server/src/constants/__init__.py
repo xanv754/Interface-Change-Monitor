e018839ad30748c9f244c726cc1e5c955314310e
Angyee Alexandra <angyeemarin.dev@gmail.com>
2025-02-14 00:37:31 -0400

    refactor: remove type

--- server/src/constants/__init__.py
+++ server/src/constants/__init__.py
@@ -5,9 +5,4 @@ from constants.types import (
     StatusType,
     InterfaceType,
 )
-from constants.prefix import (
-    VERSION_ONE_PREFIX,
-    OPERATOR_PREFIX,
-    LOGIN_PREFIX,
-)
 from constants.log import LogType
