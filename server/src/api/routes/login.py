c41eb44e581f5e74807bdbece65e234995c802a6
Angyee Alexandra <angyeemarin.dev@gmail.com>
2025-02-13 22:50:25 -0400

    feat: add errors http

--- server/src/api/routes/login.py
+++ server/src/api/routes/login.py
@@ -1,14 +1,15 @@
 from fastapi import APIRouter
 from core import SecurityController, Settings
+from errors import UNATHORIZED_USER
 from schemas import Token, LoginParams
 
 router = APIRouter()
 
 @router.post("/login")
-async def login(data: LoginParams) -> Token:
+def login(data: LoginParams) -> Token:
     user = SecurityController.authenticate_user(data.username, data.password)
     if user is None:
-        raise Exception("Invalid username or password")
+        raise UNATHORIZED_USER
     settings = Settings()
     token = SecurityController.create_access_token(
         data={
---
+++ server/src/errors/api.py
@@ -0,0 +1,38 @@
+from fastapi import HTTPException, status
+
+UNATHORIZED_USER = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Unauthorized user",
+    headers={"WWW-Authenticate": "Bearer"},
+)
+
+INVALID_TOKEN = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Invalid token",
+    headers={"WWW-Authenticate": "Bearer"},
+)
+
+INTERNAL_SERVER_ERROR = HTTPException(
+    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+    detail="An unpexpected error has occurred",
+)
+
+OPERATOR_NOT_FOUND = HTTPException(
+    status_code=status.HTTP_404_NOT_FOUND,
+    detail="Operator not found",
+)
+
+EQUIPMENT_NOT_FOUND = HTTPException(
+    status_code=status.HTTP_404_NOT_FOUND,
+    detail="Equipment not found",
+)
+
+INTERFACE_NOT_FOUND = HTTPException(
+    status_code=status.HTTP_404_NOT_FOUND,
+    detail="Interface not found",
+)
+
+ASSIGNMENT_NOT_FOUND = HTTPException(
+    status_code=status.HTTP_404_NOT_FOUND,
+    detail="Assignment not found",
+)
