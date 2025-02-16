21f6d85c0a10356429a5481069fa29393d0bc1af
Angyee Alexandra <angyeemarin.dev@gmail.com>
2025-02-16 02:18:58 -0400

    feat(constants): add documentation

--- server/src/constants/log.py
+++ server/src/constants/log.py
@@ -1,6 +1,16 @@
+"""All types of messages that can be stored in the system's .log file."""
+
 from enum import Enum
 
+
 class LogType(Enum):
+    """Type of message that can be stored in the system's .log file.
+
+    - ERROR: Error message.
+    - WARNING: Warning message.
+    - INFO: Information message.
+    """
+
     ERROR = "ERROR"
     WARNING = "WARNING"
     INFO = "INFO"
--- server/src/constants/types.py
+++ server/src/constants/types.py
@@ -1,13 +1,38 @@
+"""
+All types necessary for the system.
+
+- **AccountType:** Type of status of account of the operator.
+- **ProfileType:** Type of profile of the operator.
+- **StatusAssignmentType:** Type of status of the assignment.
+- **StatusType:** Type of status of the interface to SNMP consult.
+- **InterfaceType:** Type of interface.
+"""
+
 from enum import Enum
 
 
 class AccountType(Enum):
+    """Type of account of the operator.
+
+    - ACTIVE: Type of active account. You can use the system.
+    - INACTIVE: Type of inactive account. You cannot use the system.
+    - DELETED: Account type will soon be deleted. You cannot use the system.
+    """
+
     ACTIVE = "ACTIVE"
     INACTIVE = "INACTIVE"
     DELETED = "DELETED"
 
 
 class ProfileType(Enum):
+    """Type of profile of the operator.
+
+    - ROOT: Type of root profile. Has full privileges in the system.
+    - ADMIN: Type of admin profile. You have permissions to assign and view all statistics in the system.
+    - STANDARD: Type of standard profile. Basic user, who can only receive assignments and view his own statistics.
+    - SOPORT: Type of support profile. Has full privileges in the system plus some for system maintenance.
+    """
+
     ROOT = "ROOT"
     ADMIN = "ADMIN"
     STANDARD = "STANDARD"
@@ -15,12 +40,21 @@ class ProfileType(Enum):
 
 
 class StatusAssignmentType(Enum):
+    """Type of status of an assignment.
+
+    - PENDING: Type of pending assignment. The assignment has not been reviewed
+    - INSPECTED: Type of inspected assignment. The assignment has been reviewed, but does not require rediscovery.
+    - REDISCOVERED: Type of rediscovered assignment. The assignment has been reviewed, and requires rediscovery.
+    """
+
     PENDING = "PENDING"
     INSPECTED = "INSPECTED"
     REDISCOVERED = "REDISCOVERED"
 
 
 class StatusType(Enum):
+    """Type of status of the interface in an SNMP consult."""
+
     UP = "UP"
     DOWN = "DOWN"
     TESTING = "TESTING"
@@ -32,5 +66,11 @@ class StatusType(Enum):
 
 
 class InterfaceType(Enum):
+    """Type of an interface.
+
+    - NEW: Interface with the data of the last SNMP consult.
+    - OLD: Interface with the data of the previous SNMP consult.
+    """
+
     NEW = "NEW"
     OLD = "OLD"
