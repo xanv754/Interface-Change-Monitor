6605afe1f77269a7db66187459d1188bb0de145c
Angxandralol <angyeemarin.dev@gmail.com>
2025-02-17 12:47:43 -0400

    test: add new testing to config_test

--- server/src/test/default/configSystem.py
+++ server/src/test/default/configSystem.py
@@ -21,12 +21,47 @@ DEFAULT = {
         "ifName": True,
         "ifDescr": True,
         "ifAlias": True,
-        "ifSpeed": True,
+        "ifSpeed": False,
         "ifHighSpeed": True,
         "ifPhysAddress": False,
         "ifType": False,
         "ifOperStatus": True,
         "ifAdminStatus": True,
+        "ifPromiscuousMode": False,
+        "ifConnectorPresent": False,
+        "ifLastCheck": False
+    }
+}
+
+ALTERNATIVE = {
+    "canAssign": {
+        "ROOT": True,
+        "ADMIN": True,
+        "STANDARD": False,
+        "SOPORT": False
+    },
+    "canReceiveAssignment": {
+        "ROOT": False,
+        "ADMIN": True,
+        "STANDARD": True,
+        "SOPORT": False
+    },
+    "viewAllStatistics": {
+        "ROOT": True,
+        "ADMIN": True,
+        "STANDARD": False,
+        "SOPORT": True
+    },
+    "notificationChanges": {
+        "ifName": False,
+        "ifDescr": False,
+        "ifAlias": False,
+        "ifSpeed": False,
+        "ifHighSpeed": False,
+        "ifPhysAddress": False,
+        "ifType": False,
+        "ifOperStatus": False,
+        "ifAdminStatus": False,
         "ifPromiscuousMode": False,
         "ifConnectorPresent": False,
         "ifLastCheck": False
