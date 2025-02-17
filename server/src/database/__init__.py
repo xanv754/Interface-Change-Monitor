fb1fb5ee312697ddf17fcb35cc5e74e4b207d939
Angyee Alexandra <angyeemarin.dev@gmail.com>
2025-02-17 19:49:09 -0400

    refactor(database): add postgres database module splitting

--- server/src/database/__init__.py
+++ server/src/database/__init__.py
@@ -1,11 +1,11 @@
-from database.constants.tables import Tables as GTABLES  # Global Tables
-from database.schemas.equipment import EquipmentSchemaDB
-from database.schemas.interface import InterfaceSchemaDB
-from database.schemas.operator import OperatorSchemaDB
-from database.schemas.assignment import AssignmentSchemaDB
-from database.tables.equipment import TABLE_SCHEMA_EQUIPMENT
-from database.tables.interface import TABLE_SCHEMA_INTERFACE
-from database.tables.operator import TABLE_SCHEMA_OPERATOR
-from database.tables.assignment import TABLE_SCHEMA_ASSIGNMENT
-from database.database import PostgresDatabase
-from psycopg2 import errors
+from database.postgres.constants.tables import Tables as GTABLES  # Global Tables
+from database.postgres.schemas.equipment import EquipmentSchemaDB
+from database.postgres.schemas.interface import InterfaceSchemaDB
+from database.postgres.schemas.operator import OperatorSchemaDB
+from database.postgres.schemas.assignment import AssignmentSchemaDB
+from database.postgres.tables.equipment import TABLE_SCHEMA_EQUIPMENT
+from database.postgres.tables.interface import TABLE_SCHEMA_INTERFACE
+from database.postgres.tables.operator import TABLE_SCHEMA_OPERATOR
+from database.postgres.tables.assignment import TABLE_SCHEMA_ASSIGNMENT
+from database.postgres.database import PostgresDatabase
+
