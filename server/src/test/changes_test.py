bb8da06dcc4c8108fd5b5a25d23536bb9e63c7b6
Angxandralol <angyeemarin.dev@gmail.com>
2025-02-18 09:54:07 -0400

    test: add new testing to changes test

--- server/src/test/changes_test.py
+++ server/src/test/changes_test.py
@@ -4,17 +4,44 @@ from test import DefaultChanges
 
 class TestChanges(unittest.TestCase):
     def test_get_change(self):
-        data_changes = DefaultChanges.new_insert()
+        data_changes = DefaultChanges.get_changes()
         changes = Changes(id=1, changes=data_changes[0])
         current_change = changes.get_changes()
-        self.assertEqual(type(current_change), dict)
+        self.assertEqual(type(current_change), str)
 
     def test_register_changes(self):
-        changes = DefaultChanges.new_insert()
+        DefaultChanges.clean_table()
+        changes = DefaultChanges.get_changes()
         self.assertEqual(type(changes), list)
+        self.assertEqual(len(changes), 1)
         changes = Changes(id=1, changes=changes[0])
         status = changes.register()
         self.assertEqual(status, True)
+        DefaultChanges.clean_table()
+
+    def test_get_all_changes(self):
+        DefaultChanges.clean_table()
+        new_changes = DefaultChanges.get_changes()
+        status = DefaultChanges.new_insert(id=1, changes=new_changes[0])
+        self.assertEqual(status, True)
+        changes = Changes.get_all_changes()
+        self.assertEqual(type(changes), list)
+        self.assertEqual(len(changes), 1)
+        self.assertEqual(changes[0].ip, new_changes[0].ip)
+        self.assertEqual(changes[0].community, new_changes[0].community)
+        DefaultChanges.clean_table()
+
+    def test_reset_changes(self):
+        DefaultChanges.clean_table()
+        new_changes = DefaultChanges.get_changes()
+        status = DefaultChanges.new_insert(id=1, changes=new_changes[0])
+        self.assertEqual(status, True)
+        status = Changes.reset_changes()
+        self.assertEqual(status, True)
+        changes = Changes.get_all_changes()
+        self.assertEqual(type(changes), list)
+        self.assertEqual(len(changes), 0)
+        
 
 
 if __name__ == "__main__":  
--- server/src/test/default/changes.py
+++ server/src/test/default/changes.py
@@ -1,13 +1,21 @@
+import json
+import redis
+from os import getenv
 from typing import List
+from dotenv import load_dotenv
 from constants import InterfaceType
-from schemas import ChangesSchema
+from database import GKEYS
+from schemas import ChangesSchema, ChangesJson
 from system import DetectChanges
 from test import constants, DefaultEquipment, DefaultInterface
 
+load_dotenv(override=True)
+
+URI_REDIS_TEST = getenv("URI_REDIS_TEST")
 
 class DefaultChanges:
     @staticmethod
-    def new_insert() -> List[ChangesSchema]:
+    def get_changes() -> List[ChangesSchema]:
         date = constants.DATE_CONSULT
         new_equipment = DefaultEquipment.new_insert()
         DefaultInterface.new_insert(
@@ -26,3 +34,55 @@ class DefaultChanges:
         change_controller = DetectChanges()
         changes = change_controller.get_changes(date=date)
         return changes
+    
+    @staticmethod
+    def clean_table() -> None:
+        try:
+            database = redis.Redis.from_url(URI_REDIS_TEST)
+            cursor, keys = database.scan(cursor=0, match=f"{ChangesJson.ID.value}:*")
+            while True:
+                for key in keys:
+                    database.delete(key)
+                if cursor == 0:
+                    break
+                cursor, keys = database.scan(cursor=cursor, match=f"{ChangesJson.ID.value}:*")
+            database.close()
+        except Exception as e:
+            print(e)
+    
+    @staticmethod
+    def new_insert(id: int, changes: ChangesSchema) -> bool:
+        try:
+            id = str(id)
+            changes = json.dumps(changes.model_dump())
+            database = redis.Redis.from_url(URI_REDIS_TEST)
+            database.hset(f"{GKEYS.CHANGES.value}:{ChangesJson.ID.value}:{id}", mapping={
+                "changes": changes
+            })
+            database.close()
+        except Exception as e:
+            print(e)
+            return False
+        else:
+            return True
+        
+    @staticmethod
+    def get_all_changes() -> List[ChangesSchema]:
+        try:
+            database = redis.Redis.from_url(URI_REDIS_TEST)
+            cursor, keys = database.scan(cursor=0, match=f"{GKEYS.CHANGES.value}:{ChangesJson.ID.value}:*")
+            changes: List[ChangesSchema] = []
+            while True:
+                for key in keys:
+                    data = database.hgetall(key)
+                    data_decoded = {k.decode('utf-8'): v.decode('utf-8') for k, v in data.items()}
+                    changes_data = json.loads(data_decoded[ChangesJson.CHANGES.value])
+                    change = ChangesSchema(**changes_data)
+                    changes.append(change)
+                if cursor == 0:
+                    break
+                cursor, keys = database.scan(cursor=cursor, match=f"{ChangesJson.ID.value}:*")
+            database.close()
+            return changes
+        except Exception as e:
+            print(e)
