71f5945a3aad6cad8f4c5b10ff7af428482915de
Angxandralol <angyeemarin.dev@gmail.com>
2025-03-14 15:05:02 -0400

    fix: delete files temporal and get good sysname

--- server/SNMP/snmp.sh
+++ server/SNMP/snmp.sh
@@ -15,8 +15,35 @@ OUTPUT_IFHIGHSPEED=""$OUTPUT_ROOT"_ifHighSpeed"
 OUTPUT_IFOPERSTATUS=""$OUTPUT_ROOT"_ifOperStatus"
 OUTPUT_IFADMINSTATUS=""$OUTPUT_ROOT"_ifAdminStatus"
 
+function delete_files {
+    if [ -f $OUTPUT_SYSNAME ]; then
+        rm $OUTPUT_SYSNAME
+    fi
+    if [ -f $OUTPUT_IFINDEX ]; then
+        rm $OUTPUT_IFINDEX
+    fi
+    if [ -f $OUTPUT_IFNAME ]; then
+        rm $OUTPUT_IFNAME
+    fi
+    if [ -f $OUTPUT_IFDESCR ]; then
+        rm $OUTPUT_IFDESCR
+    fi
+    if [ -f $OUTPUT_IFALIAS ]; then
+        rm $OUTPUT_IFALIAS
+    fi
+    if [ -f $OUTPUT_IFHIGHSPEED ]; then
+        rm $OUTPUT_IFHIGHSPEED
+    fi
+    if [ -f $OUTPUT_IFOPERSTATUS ]; then
+        rm $OUTPUT_IFOPERSTATUS
+    fi
+    if [ -f $OUTPUT_IFADMINSTATUS ]; then
+        rm $OUTPUT_IFADMINSTATUS
+    fi
+}
+
 if [ -d $CURRENT_PATH ]; then
-    $COMMAND_SNMP $COMMUNITY $IP sysname | awk -F '= STRING: ' '{print $2}' >> $OUTPUT_SYSNAME
+    $COMMAND_SNMP $COMMUNITY $IP sysname | awk -F '= STRING: ' '{print substr($2, 1, 11)}' >> $OUTPUT_SYSNAME
     $COMMAND_SNMP $COMMUNITY $IP ifIndex | awk -F '= INTEGER:' '{print $2}' >> $OUTPUT_IFINDEX
     $COMMAND_SNMP $COMMUNITY $IP ifName | awk -F '= STRING:' '{print $2}' >> $OUTPUT_IFNAME
     $COMMAND_SNMP $COMMUNITY $IP ifDescr | awk -F '= STRING:' '{print $2}' >> $OUTPUT_IFDESCR
@@ -47,36 +74,36 @@ if [ -d $CURRENT_PATH ]; then
                             if [ $total_register_ifAdminStatus -eq $total_register_ifIndex ]; then
                                 sysname=$(cat $OUTPUT_SYSNAME)
                                 paste -d ';' $OUTPUT_IFINDEX $OUTPUT_IFNAME $OUTPUT_IFDESCR $OUTPUT_IFALIAS $OUTPUT_IFHIGHSPEED $OUTPUT_IFOPERSTATUS $OUTPUT_IFADMINSTATUS > ""$OUTPUT_ROOT"_$sysname"
+                                delete_files
                             else 
                                 echo "$(date +"%Y-%m-%d %H:%M:%S") ERROR[/SNMP/snmp.sh] List of ifAdminStatus does not match the ifIndex. IP: $IP, community: $COMMUNITY" >> $LOG
+                                delete_files
                             fi
                         else 
                             echo "$(date +"%Y-%m-%d %H:%M:%S") ERROR[/SNMP/snmp.sh] List of ifOperStatus does not match the ifIndex. IP: $IP, community: $COMMUNITY" >> $LOG
+                            delete_files
                         fi
                     else 
                         echo "$(date +"%Y-%m-%d %H:%M:%S") ERROR[/SNMP/snmp.sh] List of ifHighSpeed does not match the ifIndex. IP: $IP, community: $COMMUNITY" >> $LOG
+                        delete_files
                     fi
                 else 
                     echo "$(date +"%Y-%m-%d %H:%M:%S") ERROR[/SNMP/snmp.sh] List of ifAlias does not match the ifIndex. IP: $IP, community: $COMMUNITY" >> $LOG
+                    delete_files
                 fi
             else 
                 echo "$(date +"%Y-%m-%d %H:%M:%S") ERROR[/SNMP/snmp.sh] List of ifDescr does not match the ifIndex. IP: $IP, community: $COMMUNITY" >> $LOG
+                delete_files
             fi
         else 
             echo "$(date +"%Y-%m-%d %H:%M:%S") ERROR[/SNMP/snmp.sh] List of ifName does not match the ifIndex. IP: $IP, community: $COMMUNITY" >> $LOG
+            delete_files
         fi
-
-        rm $OUTPUT_SYSNAME
-        rm $OUTPUT_IFINDEX
-        rm $OUTPUT_IFNAME
-        rm $OUTPUT_IFDESCR
-        rm $OUTPUT_IFALIAS
-        rm $OUTPUT_IFHIGHSPEED
-        rm $OUTPUT_IFOPERSTATUS
-        rm $OUTPUT_IFADMINSTATUS
     else
         echo "$(date +"%Y-%m-%d %H:%M:%S") ERROR[/SNMP/snmp.sh] List of ifIndex not found. IP: $IP, community: $COMMUNITY" >> $LOG
+        delete_files
     fi
 else
     echo "$(date +"%Y-%m-%d %H:%M:%S") ERROR[/SNMP/snmp.sh] Directory of tmp response snmp not exists" >> $LOG
+    delete_files
 fi
