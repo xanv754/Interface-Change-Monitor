daadad9c20b0088d516fd2da8d1c3fe45cd670bf
Alexandra <angyeemarin.dev@gmail.com>
2025-06-11 19:29:45 -0400

    test: update unit test

--- server/test/__init__.py
+++ server/test/__init__.py
@@ -94,7 +94,7 @@ class UserDBTest:
             self.database.query_execute(
                 f"""CREATE TABLE IF NOT EXISTS {TableNames.USERS} (
                     {UserField.USERNAME} VARCHAR(20) PRIMARY KEY,
-                    {UserField.PASSWORD} VARCHAR(20) NOT NULL,
+                    {UserField.PASSWORD} VARCHAR(100) NOT NULL,
                     {UserField.NAME} VARCHAR(50) NOT NULL,
                     {UserField.LASTNAME} VARCHAR(50) NOT NULL,
                     {UserField.STATUS} VARCHAR(8) NOT NULL,
