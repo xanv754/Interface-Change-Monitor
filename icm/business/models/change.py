fe65399524a981c34cfe0726985ca954507f640e
xanv754 <angyeemarin.dev@gmail.com>
2025-07-14 09:27:54 -0400

    fix: interface & change models

--- icm/business/models/change.py
+++ icm/business/models/change.py
@@ -7,23 +7,23 @@ class ChangeModel(BaseModel):
     community_old: str
     sysname_old: str
     ifIndex_old: int
-    ifName_old: str
-    ifDescr_old: str
-    ifAlias_old: str
-    ifHighSpeed_old: str
-    ifOperStatus_old: str
-    ifAdminStatus_old: str
+    ifName_old: str | None 
+    ifDescr_old: str | None
+    ifAlias_old: str | None
+    ifHighSpeed_old: str | None
+    ifOperStatus_old: str | None
+    ifAdminStatus_old: str | None
     id_new: int
     ip_new: str
     community_new: str
     sysname_new: str
     ifIndex_new: int
-    ifName_new: str
-    ifDescr_new: str
-    ifAlias_new: str
-    ifHighSpeed_new: str
-    ifOperStatus_new: str
-    ifAdminStatus_new: str
+    ifName_new: str | None
+    ifDescr_new: str | None
+    ifAlias_new: str | None
+    ifHighSpeed_new: str | None
+    ifOperStatus_new: str | None
+    ifAdminStatus_new: str | None
     username: str | None
     name: str | None
     lastname: str | None
