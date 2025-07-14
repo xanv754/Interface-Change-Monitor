22000c2e9f782a0545ffc72e48c1185edc617e19
xanv754 <angyeemarin.dev@gmail.com>
2025-07-14 09:39:57 -0400

    feat: add load modal

--- presentation/app/dashboard/admin/page.tsx
+++ presentation/app/dashboard/admin/page.tsx
@@ -20,7 +20,7 @@ import { OperationData } from "@/utils/operation";
 
 export default function DashboardPage() {
   const modalDefault = {
-    showModal: false,
+    showModal: true,
     title: "Cargando...",
     message: "Por favor, espere",
   };
@@ -112,6 +112,7 @@ export default function DashboardPage() {
         setStatistics(response);
       });
     });
+    setModal({...modalDefault, showModal: false});
   }, []);
 
   return (
